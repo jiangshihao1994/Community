8bb0b97e7b9eac4cbad58406adf1a1efd07cb6ea
jiangshihao <381362949@qq.com>
2019-07-22 16:04:54 +0800

    add check cookies

--- src/main/java/com/jiangshihao/community/controller/IndexController.java
+++ src/main/java/com/jiangshihao/community/controller/IndexController.java
@@ -1,14 +1,38 @@
 package com.jiangshihao.community.controller;
 
+import com.jiangshihao.community.mapper.UserMapper;
+import com.jiangshihao.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Creat by 江诗灏 on  2019/7/20
  */
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            cookie.getName();
+            if(cookie.getName().equals("token")){
+                String token =cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
+
 }
